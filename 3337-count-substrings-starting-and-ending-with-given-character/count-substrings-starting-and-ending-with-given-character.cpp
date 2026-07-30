a1181873fe76426e6c7695522415ef84fc47b9de
Om Nagre <142404524+OmNagre04@users.noreply.github.com>
2026-07-30 15:24:56 +0530

    Time: 6 ms (5.02%) | Memory: 13.2 MB (27.31%) - LeetSync

---
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long countSubstrings(string s, char c) {
+        long long count = 0;
+        long long subString = 0;
+
+        for(char &ch : s){
+            if(ch == c){
+                subString += (1 + count);
+                count += 1;
+            }
+        }
+
+        return subString;
+    }
+};
